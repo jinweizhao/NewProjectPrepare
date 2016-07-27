3516cefbd59c42bd2a707c8dd08967b735423dd5
jinweizhao <maccarduo@katahoutekimac-mini-2.local>
2016-07-27 10:20:58 +0800

    init project

---
+++ NewProjectPrepareDemo/NewProjectPrepareDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProjectPrepareDemo
+//
+//  Created by KDB on 16/7/27.
+//  Copyright © 2016年 Will-Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
